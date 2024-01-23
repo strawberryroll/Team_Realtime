3e57a47d678e8bc067b0e5ec0e191bd8bbdc05a2
Ahyeon <nuyha234@gmail.com>
2024-01-23 23:22:32 +0900

    Feat: 회원가입창 추가

--- Table_List/app/src/main/java/com/example/table_list/EntryActivity.kt
+++ Table_List/app/src/main/java/com/example/table_list/EntryActivity.kt
@@ -61,5 +61,47 @@ class EntryActivity : AppCompatActivity() {
                 show()
             }
         }
+
+        //가입 화면-회원가입
+        binding.btnSignup.setOnClickListener {//로그인 버튼 눌렀을 때
+            val signupView: View = View.inflate(this, R.layout.signup, null)
+            var uidEditText2 = signupView.findViewById<EditText>(R.id.useridEditText2)
+            var pwdEditText2 = signupView.findViewById<EditText>(R.id.passwordEditText2)
+
+            if (uid.isNotEmpty())
+                uidEditText2.setText(uid)
+            if (pwd.isNotEmpty())
+                pwdEditText2.setText(pwd)
+
+            MaterialAlertDialogBuilder(this).apply {
+                setTitle("Enter signup information")
+                setView(signupView)
+                setPositiveButton("확인") { _, _ ->
+                    val enteredUid2 = uidEditText2.text.toString()
+                    val enteredPwd2 = pwdEditText2.text.toString()
+
+                    if (enteredUid2.isNotEmpty() && enteredPwd2.isNotEmpty()) {
+                        // 회원가입 성공 시
+                        Toast.makeText(this@EntryActivity, "가입완료 되셨습니다.", Toast.LENGTH_SHORT)
+                            .show()
+                    } else if (enteredUid2.isEmpty()) {
+                        Toast.makeText(this@EntryActivity, "아이디를 입력하세요.", Toast.LENGTH_SHORT)
+                            .show()
+                    } else if (enteredPwd2.isEmpty()) {
+                        Toast.makeText(
+                            this@EntryActivity,
+                            "비밀번호를 입력하세요.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+                setNegativeButton("취소") { _, _ ->
+
+                }
+                show()
+            }
+        }
+
+
     }
 }
